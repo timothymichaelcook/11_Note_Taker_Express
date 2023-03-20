dd4933aaef0961fb1f731f638fb1b8b8420dee4e
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-20 15:01:20 -0400

    declared variables for express server to run, app and PORT @ 3001

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
-// 
-
+// Required NPM packages needed for project
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json');
+
+// Express server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Runs server on local host port 3001
+app.listen(PORT, function () {
+  console.log('Server running on port ' + PORT);
+});
+
+
+
+
+
