2ba5331a3bf1dfb2850e1e492a8b6e0aacc44668
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-20 18:13:32 -0400

    Debugging server not responding

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const db_notes = require('./db/db.json');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
-
 // Parse data in JSON format
 app.use(express.json());
 app.use(express.static('public'));
@@ -24,40 +21,40 @@ app.get('/notes', function (req, res) {
 
 
 // Returns saved notes in JSON format
-
 app.get('/api/notes', function (req, res) {
   return res.json(db_notes);
 });
 
-
-//
-
+// 
 app.post('api/notes/:id', function(req, res) {
   const notes = req.body;
   notes.id = new Date().getTime().toString();
   console.log(notes);
   console.push(notes);
   fs.writeFile('./db/db.json', JSON.stringify(db_notes), (err) => {
-    if(err) throw err;
+    if (err) throw err;
     res.json(notes);
   });
 });
 
-
+// Route to delete notes
 app.delete('/api/notes/:id', function (req, res) {
   const id = req.params.id;
-  
-}
+  const index = db_notes.findIndex(function (note) {
+    return note.id === id;
+  })
+  if (index !== -1) {
+    db_notes.splice(index, 1)
+  }
+  fs.writeFileSync('/db/db.json', JSON.stringify(db_notes));
+  return res.json(id);
+});
 
 
-
-
-
-
-
-
-
-
+//
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 // Runs server on local host port 3001
 app.listen(PORT, function () {
